1c688a1afba41e67095d8d3779e28bc84b9c5016
Nibbon <sarvarijanos10@gmail.com>
2019-03-26 14:31:56 +0100

    IF

--- 002-IFy.py
+++ 002-IFy.py
@@ -1,2 +1,7 @@
-print ('Hello, World!')
-x=input()
+num = int(input())
+if num>=100 or num<=1:
+    print ("Nem ám pamutzoknyi!")
+elif (num%2==1) or (num%2==0 and 6<= num<=20):
+    print ("weird")
+elif num%2==0 and (2<=num<=5 or num>20):
+    print ("Not weird")
