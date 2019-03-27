a26ccbbb020e78585d50749193950bae4a523f66
Nibbon <sarvarijanos10@gmail.com>
2019-03-27 14:56:48 +0100

    Poop

---
+++ 005_Poop.py
@@ -0,0 +1,12 @@
+print ("n?")
+n=int(input())
+"""while n>0 or i>0 or n<i:
+    print ("Nani?")
+    print ("n?")
+    n=int(input())
+    print ("i?")
+    i=int(input())"""
+for i in range(n):
+    i==i**2
+    print (i**2)
+    
