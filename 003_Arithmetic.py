525a5a7cd32035d0bd9938460690ee0f3755d982
Nibbon <sarvarijanos10@gmail.com>
2019-03-27 11:51:43 +0100

    Arithmetic

---
+++ 003_Arithmetic.py
@@ -0,0 +1,14 @@
+print ("num1")
+num1 = int(input())
+print ("num2")
+num2 = int(input())
+while num1<=1 or num2>=(10**10):
+    print ("invalid value")
+    print ("num1")
+    num1 = int(input())
+    print ("num2")
+    num2 = int(input())
+    
+print (num1+num2)
+print (num1-num2)
+print (num1*num2)
