3f42b0324a1b57bea9930f3c743928252e8ee0ca
Nibbon <sarvarijanos10@gmail.com>
2019-03-28 07:45:44 +0100

    Leapyear

--- 006_Leapyear.py
+++ 006_Leapyear.py
@@ -1,9 +1,16 @@
 def is_leap(year):
     leap=False
-
-
-    
+    if year%4==0 and (year%100!=0 or year%400==0):
+        leap=True
     return leap
 
-year=int(input())
-print (is_leap(year))
+while True:
+    year=int(input())
+    print (is_leap(year))
+
+#teszter
+"""i=1950
+while i<=2010:
+    print (i)
+    print (is_leap(i))
+    i=i+1"""
